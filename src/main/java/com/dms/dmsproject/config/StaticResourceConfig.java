e31c498868d5333ce43b288ae757c063e1bc4018
TAMATAPUCHANDU <chandrasekhar.t@codestream.in>
2025-12-10 12:14:57 +0530

    i upadated

---
+++ src/main/java/com/dms/dmsproject/config/StaticResourceConfig.java
@@ -0,0 +1,23 @@
+package com.dms.dmsproject.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+	
+	@Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	String location = "file:" + uploadDir;
+    	
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(location);
+        // Linux: "file:/opt/dams-uploads/"
+    }
+}
+
