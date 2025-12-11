187689277605a4416a969d8bab4412ed07218821
Tejansh26 <saiteja.mcodestream.in>
2025-12-11 20:32:16 +0530

    the registration and login are working properly

---
+++ src/main/java/com/dms/dmsproject/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.dms.dmsproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dms.dmsproject.model.UserRegistration;
+import com.dms.dmsproject.service.LoginServices;
+
+@RestController
+@RequestMapping("/loginpage")
+public class LoginController {
+	@Autowired
+	public LoginServices loginservices;
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> loginSave(@RequestBody UserRegistration usreg) {
+		
+		 UserRegistration existingUser = loginservices.findByEmail(usreg.getUserEmailId());
+		 
+		 if (existingUser == null) {
+		        return ResponseEntity
+		                .status(HttpStatus.BAD_REQUEST)
+		                .body("Email not registered! Please register first.");
+		    }
+//		 String otp = String.valueOf((int)(Math.random() * 4000) + 100000);
+//		    existingUser.setUserOtp(otp);
+		    
+		 else{ loginservices.save(existingUser);
+		 
+		 return ResponseEntity.ok("OTP sent successfully: " );
+		 
+		 }
+		
+		 
+	}		
+
+}
--- src/main/java/com/dms/dmsproject/controller/RegistrationController.java
+++ src/main/java/com/dms/dmsproject/controller/RegistrationController.java
@@ -3,6 +3,8 @@ package com.dms.dmsproject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +23,22 @@ public class RegistrationController {
 	private RegistrationService registrationservice;
 	
 	@PostMapping("/regsave")
-	public UserRegistration saveuser(@RequestBody UserRegistration user) {
+	public ResponseEntity<?> saveUser(@RequestBody UserRegistration user) {
+		//identify the given email is there or not 
+		//if email is exist return error responce
+		//if email is not exist store the data in db return sucess responce 
+		
+		   if (registrationservice.existsByEmail(user.getUserEmailId())) {
+		        return ResponseEntity
+		                .status(HttpStatus.BAD_REQUEST)
+		                .body("Email already exists! Try another email.Please proceed with login");
+		    }
+		   
 		UserRegistration reg = registrationservice.save(user);
-		return reg;
+		return ResponseEntity
+	            .status(HttpStatus.OK)
+	            .body(reg);
+
 	}
 	
 	@GetMapping("/")
